30e6ca98ae04d00dc1b312916f1e39d03062f4f1
TchappiR <tchappiromaric@gmail.com>
2024-12-18 16:49:56 +0100

    increase security in to input of color by a player

--- main.py
+++ main.py
@@ -6,16 +6,22 @@ party_counter = 1
 player_color_list = []
 
 #creation de la liste secrete à deviner par le joueur
-
-secret_color_list = random.sample(general_color_list, 4)
-print(secret_color_list)
+def generate_secret_list():
+    return random.sample(general_color_list, 4)
+#print(secret_color_list)
 
 # Fonction de création de la liste couleur du joueur
-def create_player_list():  # sourcery skip: inline-immediately-returned-variable
-    player_list = [input(f"Entrez la couleur {color_rang} : ").lower() for color_rang in range(1, 5)]
-    return player_list
+def create_player_list():  
+    return [
+        input(f"Entrez la couleur {color_rang} : ").lower().replace(" ", "")
+        for color_rang in range(1, 5)
+            
+    ]
 
-# Verification des couleurs du joueur
+
+# Creation de la partie
+secret_color_list = generate_secret_list()
+
 while party_counter < max_party_number and player_color_list != secret_color_list:
     
     correct_color_position = 0
@@ -30,14 +36,15 @@ while party_counter < max_party_number and player_color_list != secret_color_lis
         for index_secret_color, secret_color in enumerate(secret_color_list):
             if index_player_color == index_secret_color and player_color == secret_color:
                 correct_color_position += 1
+                break
             elif index_player_color != index_secret_color and player_color == secret_color:
                 bad_color_position += 1
     
     party_counter += 1
     print('_'*65,'\n')
-    print(f"Vous avez :\n\t{correct_color_position} coleurs placées au bon endroit")
+    print(f"Vous avez :\n\t* {correct_color_position}")
     if correct_color_position < 4:
-        print(f"\t{bad_color_position} coleurs correctes mais placées au mauvais endroit.")
+        print(f"\t- {bad_color_position}")
     print(f"\nEssai {party_counter} sur {max_party_number}. Il vous reste {max_party_number - party_counter} essais.\n")
 
 
@@ -45,4 +52,4 @@ if player_color_list == secret_color_list:
     print("Bravo vous avez trouvez la liste secrète...")
 
 else :
-    print("Vous avez perdu la partie...")
+    print("Vous avez perdu la partie...\nLa combinaison secrète etait {secret_color_list}")
